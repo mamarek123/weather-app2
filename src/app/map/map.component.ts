34db8b4e8637568313a7b211157c3bbc3159eefd
kaczo <kaczoreks121@gmail.com>
2024-12-12 00:34:28 +0100

    fiix longitude bug

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -26,6 +26,13 @@ export class MapComponent implements AfterViewInit {
 
     this._map.on('click', (event) => {
       const coords: LatLng = event.latlng;
+      //mapa  leaflet po przeskrolowaniu dodaje koordynaty zamiast je resetowac, przyklad po przunieciu calej mapy w prawo kooridynat longitude bedzie w zakresie 180-360 a lewo od -360 do -180
+      //nakezy zresetowac koordynaty robiac modulo 180 i jesli przesunelismy w lewo to jeszcze dodac 180
+      if (coords.lng <= 0 && coords.lng % 180 != coords.lng) {
+        coords.lng = (coords.lng % 180) + 180;
+      } else if (coords.lng % 180 !== coords.lng) {
+        coords.lng = coords.lng % 180;
+      }
       this.weatherEnergyService.fetchForecastAndSummary(coords.lng, coords.lat);
     });
   }
