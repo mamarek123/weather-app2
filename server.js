6cf8cc987f4947f8390366a24c4de9fd2f5f8de5
kaczo <kaczoreks121@gmail.com>
2024-12-11 23:14:31 +0100

    fix project name

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 const port = process.env.PORT || 4200;
 
-app.use(express.static(__dirname + '/dist/weather-app-front/browser'));
+app.use(express.static(__dirname + '/dist/weather-app2/browser'));
 
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 
