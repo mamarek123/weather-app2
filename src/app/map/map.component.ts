41849e03e0a251745ce90a88199c64067df785db
kaczo <kaczoreks121@gmail.com>
2024-12-12 00:52:29 +0100

    second lng fix

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Map, map, tileLayer, LatLng } from 'leaflet';
 import { WeatherEnergyService } from '../service/weatherEnergy.service';
+import e from 'express';
 
 @Component({
   selector: 'app-map',
@@ -27,11 +28,14 @@ export class MapComponent implements AfterViewInit {
     this._map.on('click', (event) => {
       const coords: LatLng = event.latlng;
       //mapa  leaflet po przeskrolowaniu dodaje koordynaty zamiast je resetowac, przyklad po przunieciu calej mapy w prawo kooridynat longitude bedzie w zakresie 180-360 a lewo od -360 do -180
-      //nakezy zresetowac koordynaty robiac modulo 180 i jesli przesunelismy w lewo to jeszcze dodac 180
-      if (coords.lng <= 0 && coords.lng % 180 != coords.lng) {
-        coords.lng = (coords.lng % 180) + 180;
-      } else if (coords.lng % 180 !== coords.lng) {
-        coords.lng = coords.lng % 180;
+      if (coords.lng > 180) {
+        while (coords.lng > 180) {
+          coords.lng -= 360;
+        }
+      } else if (coords.lng < -180) {
+        while (coords.lng < -180) {
+          coords.lng += 360;
+        }
       }
       this.weatherEnergyService.fetchForecastAndSummary(coords.lng, coords.lat);
     });
